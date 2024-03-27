c006838f4769c46ae1bdc0db320e2713811bf0f9
hvillase <hernani.vr@gmail.com>
2024-03-26 20:55:44 -0600

    belnd

---
+++ hydra/clase4-blend.js
@@ -0,0 +1,8 @@
+//Descomenta cada línea y declara para ver qué ace cda función de Blend 
+//osc(10, 0.1, 1).add(shape(), 0.5).out()
+//osc(10, 0.1, 1).sub(shape(), 0.1).out()
+//osc(10, 0.1, 1).blend(shape(), 0.1).out()
+//osc(30).layer(shape(3).rotate(1).luma(0.5)).out()
+//osc(20, 0.1, 1).mult(shape(3), 0.8).out()
+//osc(20, 0.1, 1).diff(shape(3)).out()
+//osc(20, 0.1, 1).mask(shape(3)).out()
