87b289313cb58a21a914dad3c2d5e67475557f7e
hvillase <hernani.vr@gmail.com>
2024-04-02 23:22:23 -0600

    calse5

---
+++ hydra/clase5-init.js
@@ -0,0 +1,16 @@
+//añade diferentes medios
+//este código es para Pulsar
+
+//s0.initCam()
+
+//src(s0).out()
+//src(s0).color(2,0,9).sub(osc().add(shape().scale(2))).modulateScrollY(osc(1.1),1.2).out()
+
+//s0.initImage("/home/hernani/uea-calp/clase-6/media/foto1.jpg")
+//src(s0).color(3,[2, 0.5, 0.1].fast(2),0.2).modulate(osc(),0.01).out()
+
+//s0.initVideo("/home/hernani/uea-calp/clase-6/media/video1.mp4")
+//src(s0).out()
+
+//s0.initScreen()
+//src(s0).out()
